d7a910ef13a76b63d74e115d0c90231b34045c81
VasanthiAbbavaram <150452647+VasanthiAbbavaram@users.noreply.github.com>
2024-05-17 18:44:08 +0530

    Add files via upload

---
+++ Java Coding Challenge/src/main/java/com/java/exceptions/AssetNotMaintainException.java
@@ -0,0 +1,8 @@
+package com.java.AssetManagement.myexceptions;
+
+public class AssetNotMaintainException extends Exception {
+	
+    public AssetNotMaintainException(String message) {
+        super(message);
+    }
+}
